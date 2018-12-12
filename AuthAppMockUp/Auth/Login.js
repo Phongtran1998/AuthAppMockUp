e58bc02a17eae0ad64801ba1346edf73b7d42350
Phongtran1998 <hoang_phong98@me.com>
2018-12-13 01:16:06 +0200

    Push my project

---
+++ AuthAppMockUp/Auth/Login.js
@@ -0,0 +1,45 @@
+function nav() {
+  location.href = "SignUp.html";
+}
+function onClick() {
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then(function() {
+      location.href = "User.html";
+    })
+    .catch(function() {
+      $("#login").text("Wrong email or password");
+    });
+}
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    location.href = "User.html";
+  }
+});
+function checkPassword() {
+  var x = document.getElementById("password");
+  var y = document.getElementById("password-check");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
+var signIPassword = document.getElementById("password");
+var signInEmail = document.getElementById("email");
+var button = document.getElementById("sign-in");
+
+enterClick(signIPassword, button);
+enterClick(signInEmail, button);
+
+function enterClick(signIn, button) {
+  signIn.addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+      button.click();
+    }
+  });
+}
---
+++ AuthAppMockUp/Auth/main.js
@@ -0,0 +1,106 @@
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    var username = firebase.auth().currentUser;
+    document.getElementById("username").innerHTML = username.email;
+    var data;
+    var key = [];
+    firebase
+      .database()
+      .ref("/user/" + username.uid)
+      .on("value", function(snapshot) {
+        const noData = snapshot.val() === null;
+        if (noData) {
+          document.getElementById("no-data-header").style.display = "block";
+        } else if (!noData) {
+          document.getElementById("data-list").style.display = "block";
+          data = Object.keys(snapshot.val());
+          data.map(function(value) {
+            key.push(value);
+            firebase
+              .database()
+              .ref("/user/" + username.uid + "/" + value)
+              .on("value", function(snapshot) {
+                $("#data-list").append(
+                  "<div class='content'><span class='header-text'><b>Name: </b></span><span class='info'>" +
+                    snapshot.val().Name +
+                    "</span><br><span class='header-text'><b>Phone: </b></span><span class='info'>" +
+                    snapshot.val().Phone +
+                    "<br><button type=button class='edit'>Edit</button></div>"
+                );
+              });
+          });
+          var buttonList = document.querySelectorAll(".edit");
+          for (var i = 0; i < buttonList.length; i++) {
+            buttonList[i].id = key[i];
+          }
+          data.map(function(value) {
+            document.getElementById(value).onclick = function() {
+              $(".hover_bkgr_fricc1").show();
+              $(".popupCloseButton").click(function() {
+                $(".hover_bkgr_fricc1").hide();
+              });
+              firebase
+                .database()
+                .ref("/user/" + username.uid + "/" + value)
+                .on("value", function(snap) {
+                  const value = snap.val();
+                  const editName = document.getElementById("edit-name").value;
+                  const editPhone = document.getElementById("edit-phone").value;
+                  editName = value.Name;
+                  editPhone = value.Phone;
+                  document.getElementById("edit-button").onclick = function() {
+                    if (
+                      editName === value.Name &&
+                      editPhone.value === value.Phone
+                    ) {
+                      alert("You haven't made any changes");
+                    } else {
+                      firebase
+                        .database()
+                        .ref("/user/" + username.uid + "/" + value)
+                        .set({
+                          Name: editName.value,
+                          Phone: editPhone.value
+                        });
+                      document.location.reload(true);
+                    }
+                  };
+                });
+              document.getElementById("delete-button").onclick = function() {
+                if (confirm("Are you sure?")) {
+                  firebase
+                    .database()
+                    .ref("/user/" + username.uid + "/" + value)
+                    .remove()
+                    .then(function() {
+                      document.location.reload(true);
+                    });
+                }
+              };
+            };
+          });
+        }
+      });
+    document.getElementById("add-button").onclick = function() {
+      const name = document.getElementById("name").value;
+      const phone = document.getElementById("phone").value;
+      firebase
+        .database()
+        .ref("user/" + username.uid)
+        .push({
+          Name: name,
+          Phone: phone
+        });
+      $(".hover_bkgr_fricc").hide();
+      document.location.reload(true);
+    };
+    document.getElementById("sign-out").onclick = function() {
+      firebase
+        .auth()
+        .signOut()
+        .then(function() {
+          location.href = "index.html";
+        });
+    };
+  }
+});
